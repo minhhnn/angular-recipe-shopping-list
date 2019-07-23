ee233ce0c60bccfd68be8a599cd1b3f59d39168d
Minh Huynh <minhhnn@gmail.com>
2019-07-23 20:47:09 +1000

    Adding recipe service

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,12 @@
+import {Recipe} from './recipe.model';
+
+export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+    new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+  ];
+
+  getServices() {
+    return this.recipes.slice();
+  }
+}
