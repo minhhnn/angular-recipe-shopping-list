26dbe0cb380cc4cf007a72acd7f8e990b868cf92
Minh Huynh <minhhnn@gmail.com>
2019-07-31 21:47:31 +1000

    Update recipe-edit component, update route

--- src/app/recipe-list/recipe-list.component.ts
+++ src/app/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Recipe} from '../recipes/recipe.model';
 import {RecipeService} from '../recipes/recipe.service';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-recipe-list',
@@ -11,9 +12,15 @@ export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[];
 
-  constructor(private recipeService: RecipeService) { }
+  constructor(private recipeService: RecipeService,
+              private router: Router,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.recipes = this.recipeService.getServices();
+  }
+
+  createNewRecipe() {
+    this.router.navigate(['new'], {relativeTo: this.route });
   }
 }
