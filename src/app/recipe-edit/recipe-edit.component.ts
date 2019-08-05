9a474c59467d71cf2f0cb4546c48b54c40f6dcb6
Minh Huynh <minhhnn@gmail.com>
2019-08-05 20:09:38 +1000

    Update recipe edit form

--- src/app/recipe-edit/recipe-edit.component.ts
+++ src/app/recipe-edit/recipe-edit.component.ts
@@ -27,6 +27,15 @@ export class RecipeEditComponent implements OnInit {
     console.log(this.recipeForm);
   }
 
+  onAddIngredient() {
+    (<FormArray>this.recipeForm.get('ingredients')).push(
+      new FormGroup({
+        'name': new FormControl(),
+        'amount': new FormControl()
+      })
+    );
+  }
+
   getControls(): AbstractControl[] {
     return (<FormArray>this.recipeForm.get('ingredients')).controls;
   }
